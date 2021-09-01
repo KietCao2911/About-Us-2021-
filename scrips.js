6849b2bf6b913585e3fdf59241018db6892dbd41
MyCode <truongkiet.hn288@gmail.com>
2021-09-01 15:27:24 +0700

    update

--- scrips.js
+++ scrips.js
@@ -14,14 +14,25 @@ geticonMenu.onclick=(e)=>{
 	getBody.classList.remove('show')
 	}
 	getBody.onclick=(e)=>{
-		if(e.target==getMenumb){
-			return ;
-		}
-		else{
-		getMenumb.classList.remove('slide')
-		getBody.classList.remove('show')
-		}
-
+			// let boolen=0;
+			// for(let i=0;i<getMenumb.children.length;i++){
+			// 			if(e.target==getMenumb.children[i]){
+			// 				boolen=1;
+			// 			}
+			// 			else{
+			// 				boolen=0;
+			// 			}
+			// }
+			// console.log(boolen)
+			if(e.target.tagName=='LI'||e.target.tagName=='UL'||e.target.tagName=='A'){
+				// getMenumb.classList.remove('slide')
+				// getBody.classList.remove('show')
+				return ;
+			}
+			else{
+				getMenumb.classList.remove('slide')
+				getBody.classList.remove('show')
+			}
 
 	}
 	
